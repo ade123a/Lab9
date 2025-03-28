3808e239fe3d996d6ad07a66f19580aceff99485
ade123a <bashirniyi@gmail.com>
2025-03-28 11:20:32 -0600

    creating files

---
+++ lab9/src/components/login.js
@@ -0,0 +1,18 @@
+function Login() {
+    
+    return (
+
+      <div>
+        <label>Username</label>
+        <input type="text" placeholder="Username" />
+        <br />
+        <label>Password</label>
+        <br />
+        <input type="password" placeholder="Password" />
+        <br />
+        <button>Login</button>
+        <h1>Login</h1>
+      </div>
+    );
+  } 
+  export default Login;
